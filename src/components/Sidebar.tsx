e087f31af03805ea72084e8a065df9e8b576d992
Nawat Ruengdechawiwat <nawat0035@gmail.com>
2026-09-05 20:11:21 +0700

    "lab12complete"

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,28 @@
 /*
- * อย่าลืมเปลี่ยน userName และ type ให้เป็นของตัวเอง
+ * อย่าลืมเปลี่ยน username และ type ให้เป็นของตัวเอง
  */
-export default function Sidebar() {
+
+interface SidebarProps {
+  username: string;
+  type: string;
+}
+
+export default function Sidebar({ username, type }: SidebarProps) {
   return (
     <aside
       className="d-flex flex-column p-4 bg-primary min-vh-100"
       data-bs-theme="dark"
     >
-      <p className="text-white">TODO: Sidebar</p>
+      <h5 className="text-white">Note App</h5>
+
+      <p className="text-white">Todo List</p>
+      <p className="text-white">My Stuffs</p>
+
+      <div className="mt-auto">
+        <p className="text-white m-0">
+          {username} : {type}
+        </p>
+      </div>
     </aside>
   );
 }
